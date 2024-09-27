86051db8ff9d7a44fac0eea7a3c7e1401a6a8eff
Jeremy Fields <jeremy.fields@viget.com>
2024-09-27 11:40:01 -0600

    Integrate basic home page content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,25 @@
 import { defineConfig } from 'astro/config';
+import storyblok from '@storyblok/astro';
+import { loadEnv } from 'vite';
 
 import tailwind from '@astrojs/tailwind';
-
 import alpinejs from '@astrojs/alpinejs';
+
+const env = loadEnv("", process.cwd(), 'STORYBLOK');
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()]
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        homepage: 'storyblok/Home',
+      },
+      apiOptions: {
+        region: 'us',
+      },
+    }),
+    alpinejs(),
+    tailwind(),
+  ],
 });
