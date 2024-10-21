24cf74e0bd0fc2ae827bb563c4c0ec1fde0b9a68
Jeremy Fields <jeremy.fields@viget.com>
2024-10-21 07:02:12 -0600

    Misc. home page and template buildout

--- src/alpine.js
+++ src/alpine.js
@@ -1,7 +1,8 @@
 import Alpine from 'alpinejs'
+import collapse from '@alpinejs/collapse'
 import persist from '@alpinejs/persist'
 
 export default () => {
-  Alpine.plugin(persist)
+  Alpine.plugin([collapse, persist])
   return Alpine
 }
