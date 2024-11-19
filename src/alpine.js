418b3bb5c537184d7044fa00ab1109e7ec31ebb7
Jeremy Fields <jeremy.fields@viget.com>
2024-11-19 11:30:07 -0700

    Add image fade in effect

--- src/alpine.js
+++ src/alpine.js
@@ -1,8 +1,9 @@
 import Alpine from 'alpinejs'
 import collapse from '@alpinejs/collapse'
+import intersect from '@alpinejs/intersect'
 import persist from '@alpinejs/persist'
 
 export default () => {
-  Alpine.plugin([collapse, persist])
+  Alpine.plugin([collapse, intersect, persist])
   return Alpine
 }
