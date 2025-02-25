9bd11cafbbc84b58d1f56c95981e54cdf52a9862
Jeremy Fields <jeremy.fields@viget.com>
2025-02-25 14:57:46 -0700

    Add pointless bear to the console

--- src/alpine.js
+++ src/alpine.js
@@ -2,8 +2,11 @@ import Alpine from 'alpinejs'
 import collapse from '@alpinejs/collapse'
 import intersect from '@alpinejs/intersect'
 import persist from '@alpinejs/persist'
+import { printBear } from './utils/pointlessBear.ts'
 
 export default () => {
   Alpine.plugin([collapse, intersect, persist])
   return Alpine
 }
+
+printBear()
